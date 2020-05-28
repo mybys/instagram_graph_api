dfef4a97374bc4270b682f74b5c4c365527fb07b
Takayuki Hasegawa <takayuki.hasegawa@gree.net>
2020-05-28 11:34:35 +0900

    Remove business account check of discover_user

--- lib/instagram_graph_api/client/discovery.rb
+++ lib/instagram_graph_api/client/discovery.rb
@@ -1,21 +1,17 @@
 module InstagramGraphApi
   class Client
     module Discovery
-      def discover_user(username, fields = nil)
+      def discover_user(ig_account_id, username, fields = nil)
         fields ||= "id,followers_count,media_count,ig_id"
-        bg_id = get_business_account_id
-        raise "No business-account found, you need to have atleast one IG business-account" unless bg_id
         query = "?fields=business_discovery.username(#{username}){#{fields}}"
-        get_connections(bg_id, query)
-      rescue Exception => e
-        puts e.message
+        get_connections(ig_account_id, query)
       end
 
-      def discover_user_media(username, fields = nil, options={})
+      def discover_user_media(ig_account_id, username, fields = nil, options={})
         fields ||= "caption,media_url,media_type,like_count,comments_count,id"
         page_options = ".after(#{options['after']})" if options["after"]
         page_options = ".after(#{options['before']})" if options["before"]
-        discover_user(username, "media#{page_options}{#{fields}}")
+        discover_user(ig_account_id, username, "media#{page_options}{#{fields}}")
       end
     end
   end
